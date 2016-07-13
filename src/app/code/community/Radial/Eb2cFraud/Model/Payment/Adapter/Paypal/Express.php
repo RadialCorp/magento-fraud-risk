10eac2bf5e8a94cfc7b432580ce6c8746736679a
root <root@mag-sandbox30.sandbox.gspt.net>
2016-07-13 15:27:25 -0400

    MPTF-169 added PaypalPayer status to transaction responses

--- src/app/code/community/Radial/Eb2cFraud/Model/Payment/Adapter/Paypal/Express.php
+++ src/app/code/community/Radial/Eb2cFraud/Model/Payment/Adapter/Paypal/Express.php
@@ -66,8 +66,18 @@ class Radial_Eb2cFraud_Model_Payment_Adapter_Paypal_Express
 	protected function _getPaypalTransactions(Mage_Payment_Model_Info $payment)
 	{
 		$info = $this->_getPaypalInfo($payment);
-		return array(
-			array('type' => 'PayPalAddress', 'response' => strtolower($info['paypal_express_checkout_address_status'])),
-		);
+		$transArray = array();
+
+                if( isset($info['paypal_express_checkout_address_status']))
+                {
+                        $transArray[] = array('type' => 'PayPalAddress', 'response' => strtolower($info['paypal_express_checkout_address_status']));
+                }
+
+                if( isset($info['paypal_express_checkout_payer_status']))
+                {
+                        $transArray[] = array('type' => 'PayPalPayer', 'response' => strtolower($info['paypal_express_checkout_payer_status']));
+                }
+
+                return $transArray;
 	}
 }
