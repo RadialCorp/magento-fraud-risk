59592402b9e768d7db533506ee0ed15d683780d1
Robert Krule <rkrule@radial.com>
2016-08-31 18:29:46 -0400

    MPTF-227

--- src/app/code/community/Radial/Eb2cFraud/Helper/Http.php
+++ src/app/code/community/Radial/Eb2cFraud/Helper/Http.php
@@ -172,6 +172,81 @@ class Radial_Eb2cFraud_Helper_Http extends Mage_Core_Helper_Http
     }
 
     /**
+     * Retrieve Client Remote Address
+     *
+     * @param bool $ipToLong converting IP to long format
+     * @return string IPv4|long
+     */
+    public function getRemoteAddr($ipToLong = false)
+    {
+        if (is_null($this->_remoteAddr)) {
+            $headers = $this->getRemoteAddrHeaders();
+            foreach ($headers as $var) {
+                if ($this->_getRequest()->getServer($var, false)) {
+                        $ip_array=explode(',',$this->_getRequest()->getServer($var, false));
+
+			foreach( $ip_array as $ipAddress )
+			{
+				//Some routers are inserting characters after the IP, trim it. -RK
+				preg_match_all('/\d{1,3}/',$ipAddress,$r);
+				$this->_remoteAddr = $r[0][0].'.'.$r[0][1].'.'.$r[0][2].'.'.$r[0][3];
+
+				if( $this->_remoteAddr )
+				{
+					if( !$this->ip_is_private($this->_remoteAddr))
+					{
+						break;
+					} else {
+						$this->_remoteAddr = '';
+					}	
+				}
+			}	
+                }
+
+		if( $this->_remoteAddr )
+		{
+			break;
+		}
+            }
+
+            if (!$this->_remoteAddr) {
+                $this->_remoteAddr = $this->_getRequest()->getServer('REMOTE_ADDR');
+            }
+        }
+
+        if (!$this->_remoteAddr) {
+            return false;
+        }
+
+        return $ipToLong ? inet_pton($this->_remoteAddr) : $this->_remoteAddr;
+    }
+
+    /**
+     * Determine if IP Address is of a private subnet
+     *
+     * @param IPv4 IP Address
+     * @return bool
+     */
+    private function ip_is_private($ip)
+    {
+        $privateAddresses = [ '10.0.0.0|10.255.255.255', '172.16.0.0|172.31.255.255', '192.168.0.0|192.168.255.255', '169.254.0.0|169.254.255.255', '127.0.0.0|127.255.255.255' ];
+        $long_ip = ip2long($ip);
+        if($long_ip != -1)
+        {
+            foreach($privateAddresses as $pri_addr)
+            {
+                list($start, $end) = explode('|', $pri_addr);
+                // IF IS PRIVATE
+                if($long_ip >= ip2long($start) && $long_ip <= ip2long($end))
+                {
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
      * return a string representation of the given cookie array
      * @return string
      */
